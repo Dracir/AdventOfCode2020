ba16bada419c2113ce739eff410ced0b5668f6c4
Richard <rail.rich@gmail.com>
2020-12-13 14:59:11 -0500

    Day13 part1

--- AoCSolver2020/Days/Day13.cs
+++ AoCSolver2020/Days/Day13.cs
@@ -14,7 +14,7 @@ public class Day13 : DayBase
 	{
 		Console.Header.ReserveLines(0);
 	}
-	
+
 	//-----------------------------------------------------------------
 
 	public override void SetUpConsolePart2()
@@ -31,19 +31,53 @@ public class Day13 : DayBase
 	//-----------------------------------------------------------------
 
 	public override bool Equals(object? obj) => base.Equals(obj);
-	public override int GetHashCode()=> base.GetHashCode();
+	public override int GetHashCode() => base.GetHashCode();
 
 	//-----------------------------------------------------------------
 
 	public override long Part1(string input)
 	{
-		return 0;
+		var inputLines = input.Split("\n");
+		var timestep = int.Parse(inputLines[0]);
+		var busFleet = new List<int>();
+		foreach (var busEntry in inputLines[1].Split(","))
+		{
+			if (int.TryParse(busEntry, out var number))
+				busFleet.Add(number);
+		}
+		busFleet.Sort();
+		var busTime = new List<int>();
+		int smalestTime = int.MaxValue;
+		int smalestBus = 0;
+		foreach (var bus in busFleet)
+		{
+			var t = (int)Math.Ceiling((double)timestep / bus) * bus;
+			Console.WriteLine($"{bus} - {t}");
+			if (smalestTime > t)
+			{
+				smalestTime = t;
+				smalestBus = bus;
+			}
+		}
+		return (smalestTime - timestep) * smalestBus;
 	}
+
 
 	//-----------------------------------------------------------------
 
 	public override long Part2(string input)
 	{
+		var inputLines = input.Split("\n");
+		var timestep = int.Parse(inputLines[0]);
+		var busFleet = new List<int>();
+		foreach (var busEntry in inputLines[1].Split(","))
+		{
+			if (int.TryParse(busEntry, out var number))
+				busFleet.Add(number);
+		}
+		busFleet.Sort();
+
+
 		return 0;
 	}
 
